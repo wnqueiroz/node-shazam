94d39d6447d18c02dba21a43fed91ff62f100ded
Will <williamdenovaisqueiroz@outlook.com>
2017-12-26 21:23:05 -0200

    Consulting the database

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,20 @@
 module.exports = function(app){
     app.get('/produtos', function(req, res){
-        res.render('produtos/lista')
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'casadocodigo'
+        });
+
+        connection.query('SELECT * FROM livros', function(err, results){
+            res.send(results);
+        });
+
+        connection.end();
+
+        // res.render('produtos/lista');
     });
     return app;
 }
