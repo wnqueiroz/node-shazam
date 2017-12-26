1c6895d5ed76d95ccc9142a89b7b36c8a18e3113
Will <williamdenovaisqueiroz@outlook.com>
2017-12-26 20:16:59 -0200

    Using EJS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/produtos', function(req, res){
-    res.send('<html><body><h1>Listagem de produtos</h1></body></html>')
+    res.render('produtos/lista')
 });
 
 app.listen(3000, function(){
