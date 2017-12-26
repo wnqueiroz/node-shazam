7976523e90e38a3457af0530a5c857820407bc68
Will <williamdenovaisqueiroz@outlook.com>
2017-12-26 19:50:02 -0200

    Creating a web server

---
+++ teste-node/produtos.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.end("<html><body><h1>Listando os produtos</h1></body></html>");
+});
+
+server.listen(3000);
+
+console.log("servidor ta rodando");
