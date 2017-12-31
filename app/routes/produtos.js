a03646f19fa5384e2c3223e5178d96e9d99ec912
Will <williamdenovaisqueiroz@outlook.com>
2017-12-31 18:49:07 -0200

    Prototype e o Data Access Object

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,9 +3,9 @@ module.exports = function (app) {
     app.get("/produtos", function (req, res) {
 
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (err, results) {
+        produtosDAO.lista(function (err, results) {
             res.render('produtos/lista', {lista: results});
         });
 
