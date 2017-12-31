6a5a1e415f4e5fcdf012977dcabac53f3a268042
Will <williamdenovaisqueiroz@outlook.com>
2017-12-31 17:10:51 -0200

    Isolando a criação da conexão com o banco de dados

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,21 +1,16 @@
-module.exports = function(app){
-    app.get('/produtos', function(req, res){
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'casadocodigo'
-        });
+var connectionFactory = require('../infra/connectionFactory');
 
-        connection.query('SELECT * FROM livros', function(err, results){
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+        var connection = connectionFactory();
+
+        connection.query('SELECT * FROM livros', function (err, results) {
             res.render('produtos/lista', {
                 lista: results
-            });            
+            });
         });
 
         connection.end();
-
     });
     return app;
 }
