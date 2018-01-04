2fed1723771bf7f225bfe2e3421c6093f74714ca
Will <williamdenovaisqueiroz@outlook.com>
2018-01-03 23:40:45 -0200

    Exibindo as mensagens de erro para o usuário

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -20,20 +20,24 @@ module.exports = function (app) {
     });
 
     app.get("/produtos/form", function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {
+            errorsValidation: {}
+        });
     });
 
     app.post("/produtos", function (req, res) {
 
         var produto = req.body;
 
-        var validatorTitle = req.assert('titulo', 'O Título do produto é obrigatório!');
-        validatorTitle.notEmpty();
+        req.assert('titulo', 'O Título do produto é obrigatório!').notEmpty();
+        req.assert('preco', 'Formato de preço inválido!').isFloat();
 
         var errors = req.validationErrors();
 
         if (errors) {
-            return res.render('produtos/form');
+            return res.render('produtos/form', {
+                errorsValidation: errors
+            });
         }
 
         var connection = app.infra.connectionFactory();
