a5a7f2913f82c11297d1b45f9107074b77f8137f
Will <williamdenovaisqueiroz@outlook.com>
2018-01-05 21:48:48 -0200

    Testando a listagem de produtos

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,27 @@
+var http = require('http');
+
 describe('ProdutosController', function () {
-    it('#listagem json', function () {
-        console.log("teste de verificação de listagem de json");
+    it('#Listagem JSON', function (done) {
+
+        var configs = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        http.get(configs, function (res) {
+
+
+            if (res.statusCode === 200) {
+                console.log(`\tStatus Code:  OK!`);
+            }
+            if (res.headers['content-type'] === 'application/json; charset=utf-8') {
+                console.log(`\tContent-Type:  OK!`);
+            }
+            done();
+        });
     });
 });
