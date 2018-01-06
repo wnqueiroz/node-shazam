93c74477ed7a8c28a477e35364d1e23dd3a0c347
Will <williamdenovaisqueiroz@outlook.com>
2018-01-05 23:19:38 -0200

    Passando os erros para o Express e criando o banco de testes

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,11 +1,14 @@
 module.exports = function (app) {
 
-    app.get("/produtos", function (req, res) {
+    app.get("/produtos", function (req, res, next) {
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function (err, results) {
+            if (err) {
+                return next(err);
+            }
             res.format({
                 html: function () {
                     res.render('produtos/lista', {lista: results});
