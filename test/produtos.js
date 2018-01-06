a0a6c39e48d685c6af371a8a4f897fd6f27dafd9
Will <williamdenovaisqueiroz@outlook.com>
2018-01-05 22:16:40 -0200

    Lidando com o servidor nos testes

--- test/produtos.js
+++ test/produtos.js
@@ -1,22 +1,11 @@
-var http = require('http'),
-    assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('ProdutosController', function () {
     it('#listagem em JSON', function (done) {
-
-        var configs = {
-            hostname: 'localhost',
-            port: 3000,
-            path: '/produtos',
-            headers: {
-                'Accept': 'application/json'
-            }
-        };
-
-        http.get(configs, function (res) {
-            assert.equal(res.statusCode, 200);
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-            done();
-        });
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 });
