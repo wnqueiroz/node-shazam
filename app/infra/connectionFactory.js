259227b0322b154494d1bfc1082628106e0c1c78
Will <williamdenovaisqueiroz@outlook.com>
2018-01-05 23:11:25 -0200

    Executando os testes em um banco exclusivo
    Comando para executar os testes com a variável de ambiente: NODE_ENV=test node_modules/mocha/bin/mocha

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,23 @@
 var mysql = require('mysql');
 
 function createDBConnection() {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'casadocodigo'
-    });
+    if (!process.env.NODE_ENV) {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'casadocodigo'
+        });
+    }
+    if (process.env.NODE_ENV == 'test') {
+
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'casadocodigo_test'
+        });
+    }
 }
 
 // wrapper
